56c5302c597dcecc21ed8a9a7e63843fdafbad8a
masahiro mark fukui <masa@pop-os.localdomain>
2020-03-22 19:02:59 +0900

    Add stopwords & special character removal

--- word_separation_test/word_tokenization.py
+++ word_separation_test/word_tokenization.py
@@ -1,9 +1,24 @@
 from nltk import word_tokenize, wordpunct_tokenize, sent_tokenize
+from collections import Counter
+from string import punctuation
+import pickle
 
 with open("test.txt", "r") as f:
     paragraphs = f.read()
+
+addtional_special_chars = ['—', '“', '”']
+stop_words = pickle.load(open("stopwords.pkl", "rb"))
+
+for symbol in addtional_special_chars:
+    punctuation += symbol
 
 wt = wordpunct_tokenize(paragraphs)
 wpt = word_tokenize(paragraphs)
 sent = sent_tokenize(paragraphs)
 
+
+wt = [word for word in wt if word not in punctuation and word not in stop_words]
+
+count_wt = Counter(wt)
+
+print(count_wt)
