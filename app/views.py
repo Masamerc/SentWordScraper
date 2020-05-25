7d995e19abe7a68b08a658b402c3dcacf4824450
Masahiro Fukui <fkmkfkmasum41@gmail.com>
2020-05-25 22:13:18 +0900

    Refactor python code: flake8

--- app/views.py
+++ app/views.py
@@ -1,14 +1,16 @@
-from app import app
-from app import r
-from app.tasks import count_words, handle_sentiment
-from app import q 
-from flask import render_template, request, redirect, url_for
-from time import strftime 
-from redis import Redis
-import time
 import pickle
+import time
+
+from app import app
+from app import q
+from app.tasks import count_words, handle_sentiment
+from flask import render_template, request, redirect, url_for
+from redis import Redis
+from time import strftime
+
 
 cache_redis = Redis(port=6378)
+
 
 @app.route('/')
 def index():
@@ -19,8 +21,8 @@ def index():
 @app.route('/add-task', methods=["GET", "POST"])
 def add_task():
     """
-    Route to a page where usrs send word-scraping requests.
-    RQ queues up a task defined in tasks.py 
+    Route to a page where usrs send word-scraping requests.\n
+    RQ queues up a task defined in tasks.py
     """
     jobs = q.jobs
     message = None
@@ -34,7 +36,6 @@ def add_task():
             message = f"Task queued at {task.enqueued_at.strftime('%a %d %b %Y %H:%M')}. {q_length} Jobs Queued"
 
             # redirects to "/result" route and pass on "url"
-            time.sleep(2)
             return redirect(url_for(".show_result", url=url))
 
     return render_template('add_task.html', message=message, jobs=jobs)
@@ -42,27 +43,35 @@ def add_task():
 
 @app.route('/result', methods=["GET", "POST"])
 def show_result():
+    """
+    Route loaded after word-scraper task\n
+    Sends data from word-scraper to result.html
+    """
     url = request.args["url"]
 
-    while cache_redis.get("ws" + url) == None:
+    while cache_redis.get("ws" + url) is None:
         time.sleep(0.5)
         if cache_redis.get("ws" + url):
             break
 
     scraper_result_data = pickle.loads(cache_redis.get("ws" + url))
 
-    top_50_words = []
+    scraped_words = []
     for data in scraper_result_data:
         ts, word, count, source = data
-        top_50_words.append((word, count))
+        scraped_words.append((word, count))
         time_stamp = ts
-    top_50_words.sort(key=lambda x: x[1], reverse=True)
+    scraped_words.sort(key=lambda x: x[1], reverse=True)
 
-    return render_template('result.html', url=url, top_50_words=top_50_words, len_top_50_words=len(top_50_words), time_stamp=time_stamp)
+    return render_template('result.html', url=url, scraped_words=scraped_words, time_stamp=time_stamp)
 
 
 @app.route('/add-sent-task', methods=["GET", "POST"])
 def add_sent_task():
+    """
+    Route to a page where usrs send sentiment-scraping requests.\n
+    RQ queues up a task defined in tasks.py
+    """
     jobs = q.jobs
     message = None
 
@@ -75,47 +84,47 @@ def add_sent_task():
             q_length = len(q)
             message = f"Task queued at {task.enqueued_at.strftime('%a %d %b %Y %H:%M')}. {q_length} Jobs Queued"
 
-            # redirects to "/result" route and pass on "url" 
+            # redirects to "/result" route and pass on "url"
             return redirect(url_for(".show_sent_result", url=url))
 
     return render_template('add_sent_task.html', message=message, jobs=jobs)
 
 
-
-############ Route for Testing ############
-
 @app.route('/sent-result', methods=["GET", "POST"])
 def show_sent_result():
+    """
+    Route to page loaded right after sent-scraper runs\n
+    Loads data from sentiment-scraper and sends it to sent_result.html
+    """
     url = request.args['url']
-    
-    while cache_redis.get("ss" + url) == None:
+
+    while cache_redis.get("ss" + url) is None:
         time.sleep(0.5)
         if cache_redis.get("ss" + url):
             break
-    
-    sentiment_list_data = pickle.loads(cache_redis.get("ss" + url))
-    print(sentiment_list_data)
 
-    top_100_sents = []
+    sentiment_list_data = pickle.loads(cache_redis.get("ss" + url))
+
+    scraped_sents = []
 
     for data in sentiment_list_data:
         sent, pos, neu, neg, compound, source, ts = data
-        top_100_sents.append((sent, compound))
+        scraped_sents.append((sent, compound))
         time_stamp = ts
 
-    top_100_sents.sort(key=lambda x: x[1], reverse=True)
-    top_100_sents = [sent_tuple for sent_tuple in top_100_sents if sent_tuple[1] != 0]
+    scraped_sents.sort(key=lambda x: x[1], reverse=True)
+    scraped_sents = [sent_tuple for sent_tuple in scraped_sents if sent_tuple[1] != 0]
 
-    total_compound = sum((row[1] for row in top_100_sents))
-    total_records = len(top_100_sents)
+    total_compound = sum((row[1] for row in scraped_sents))
+    total_records = len(scraped_sents)
     average_compound = round(total_compound / total_records, 4)
-    total_pos = [row[1] for row in top_100_sents if row[1] >= 0]
-    total_neg = [row[1] for row in top_100_sents if row[1] < 0]
+    total_pos = [row[1] for row in scraped_sents if row[1] >= 0]
+    total_neg = [row[1] for row in scraped_sents if row[1] < 0]
 
-    top_10_sents = top_100_sents[:10]
-    worst_10_sents = top_100_sents[-10:][::-1]
+    top_10_sents = scraped_sents[:10]
+    worst_10_sents = scraped_sents[-10:][::-1]
 
-    return render_template('sent_result.html', url=url, top_100_sents=top_100_sents,\
-                        time_stamp=time_stamp, average_compound=average_compound,\
-                        top_10_sents=top_10_sents, worst_10_sents=worst_10_sents, total_records=total_records,\
-                        pos_length=len(total_pos), neg_length=len(total_neg))
+    return render_template('sent_result.html', url=url, scraped_sents=scraped_sents,
+                           time_stamp=time_stamp, average_compound=average_compound,
+                           top_10_sents=top_10_sents, worst_10_sents=worst_10_sents, total_records=total_records,
+                           pos_length=len(total_pos), neg_length=len(total_neg))
