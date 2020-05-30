d563640cbd8c94fe16fa4f91b12937a84cef6bc6
Masahiro Fukui <fkmkfkmasum41@gmail.com>
2020-05-30 11:58:15 +0900

    Add wordclous

--- start_rqworker_redis.sh
+++ start_rqworker_redis.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# create crontab
+crontab -l > mycron
+echo "0 */12 * * * rm -rf /home/masa/Projects/SentWordScraper/app/static/images/wordcloud_images/*" >> mycron
+
+# install crontab
+crontab mycron
+rm mycron
+
 # start task-queue redis in docker container
 echo "Task Queue Redis container: redis1 started..."
 docker start redis1
@@ -10,3 +18,4 @@ docker start redis0
 # start worker node for task queue
 echo "Starting Redis Queue Worker..."
 rq worker
+
