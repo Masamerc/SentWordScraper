93dc3351447c0b076d8ddbae7e1c90a8078d8e03
Masa Mark <fkmkfkmasum41@gmail.com>
2020-06-20 02:23:53 +0000

    Add gunicorn

--- README.md
+++
@@ -1,9 +0,0 @@
-# SentWordScraper - without Docker
-
-*for simplicity, this version uses a single redis server
-
-<center>
-
-![logo](/app/static/images/logo.png)
-
-</center>
--- startup.sh
+++ startup.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-
-# create crontab
-# crontab -l > mycron
 # echo "0 */12 * * * rm -rf /home/masa/Projects/SentWordScraper/app/static/images/wordcloud_images/*" >> mycron
 
-# install crontab
-# crontab mycron
-# rm mycron
-
-# run & create redis docker 
-# docker run -d -p 6379:6379 --name redis1 redis
 
 # start task-queue redis in docker container
 echo "Task Queue Redis container: redis1 started..."
 docker start redis1
 
+# gunicorn
+gunicorn -w 3 run:app --daemon
+
 # start worker node for task queue
 echo "Starting Redis Queue Worker..."
-rq worker
+rq worker && gunicorn -w 3 run:app
