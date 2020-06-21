1bb4c6f2f5ed361cd63fd16809e6002b513ffe6e
Masahiro Fukui <fkmkfkmasum41@gmail.com>
2020-06-21 18:18:20 +0900

    Fixed Footer

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-# echo "0 */12 * * * rm -rf /home/masa/Projects/SentWordScraper/app/static/images/wordcloud_images/*" >> mycron
 
-
-# start task-queue redis in docker container
 echo "Task Queue Redis container: redis1 started..."
 docker start redis1
 
@@ -11,4 +7,5 @@ gunicorn -w 3 run:app --daemon
 
 # start worker node for task queue
 echo "Starting Redis Queue Worker..."
-rq worker && gunicorn -w 3 run:app
+rq worker 
+
